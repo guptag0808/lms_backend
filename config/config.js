58c36ef6ae1457a56985aeab4365216e98a5226d
gupta <saurabh4kaj@gmail.com>
2024-06-15 13:23:26 +0530

    all setup is done

--- config/config.js
+++ config/config.js
@@ -1,9 +1,18 @@
-
 const { Sequelize } = require('sequelize');
-require('dotenv').config()
-const sequelize = new Sequelize(process.env.Database,process.env.user_name , process.env.Password, {
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.Database, process.env.user_name, process.env.Password, {
   host: process.env.Host,
   dialect: 'mysql',
+  port: process.env.SQL_PORT,
 });
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
